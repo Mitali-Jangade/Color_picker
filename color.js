4fedef7ab721e2a571d33bce11ec5da8dc69d803
Mitali Jangade <mitali.jangade24@gmail.com>
2024-08-12 14:10:26 +0530

    Add files via upload

---
+++ color.js
@@ -0,0 +1,34 @@
+let changeColorEl = document.getElementById("changeColor");
+let colorEl = document.getElementById("color");
+
+function whiteColor()
+{
+   
+    changeColorEl.style.backgroundColor = "lightgrey";
+    colorEl.textContent = "#e0e0e0";
+
+}
+
+function greenColor()
+{
+    changeColorEl.style.backgroundColor = "lightgreen";
+    colorEl.textContent = "#06fcf97";
+ 
+    
+}
+
+function blueColor()
+{
+    changeColorEl.style.backgroundColor = "lightblue";
+    colorEl.textContent = "#56ccf2";
+
+    
+}
+
+function purpleColor()
+{
+    changeColorEl.style.backgroundColor = "purple";
+    colorEl.textContent = "#bb6bd9";
+
+    
+}
